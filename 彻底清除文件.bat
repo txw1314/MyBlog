e60236374046c877a496958d8d859864267e5278
上杉九月 <13562008871@163.com>
2022-05-10 00:18:51 +0800

    change

--- 彻底清除文件.bat
+++ 彻底清除文件.bat
@@ -1,16 +1,20 @@
 G:
 rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Butterfly
 rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Shoka
+rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Fluid
 
 rmdir /s /q G:\Blog\Hexo_Lite\Butterfly\source\_posts
 rmdir /s /q G:\Blog\Hexo_Lite\Shoka\source\_posts
+rmdir /s /q G:\Blog\Hexo_Lite\Fluid\source\_posts
 
 md G:\Blog\Hexo_Lite\Butterfly\source\_posts
 md G:\Blog\Hexo_Lite\Shoka\source\_posts
+md G:\Blog\Hexo_Lite\Fluid\source\_posts
 
-copy G:\Blog\Hexo\source\_posts G:\Blog\Article\Normal
+xcopy /s /q G:\Blog\Hexo\source\_posts G:\Blog\Article\Normal
 
-copy G:\Blog\Article\Normal G:\Blog\Hexo_Lite\Butterfly\source\_posts
+xcopy /s /q G:\Blog\Article\Normal G:\Blog\Hexo_Lite\Butterfly\source\_posts
+xcopy /s /q G:\Blog\Article\Shoka G:\Blog\Hexo_Lite\Fluid\source\_posts
 xcopy /s /q G:\Blog\Article\Shoka G:\Blog\Hexo_Lite\Shoka\source\_posts
 
 pause
