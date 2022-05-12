cdae299cc83683b6bbe9a6b6962e8ea601294254
上杉九月 <13562008871@163.com>
2022-05-12 19:57:00 +0800

    Next

--- 彻底清除文件.bat
+++ 彻底清除文件.bat
@@ -2,19 +2,21 @@ G:
 rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Butterfly
 rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Shoka
 rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Fluid
+rmdir /s /q G:\Blog\Hexo\themes\volantis\source\Next
 
 rmdir /s /q G:\Blog\Hexo_Lite\Butterfly\source\_posts
 rmdir /s /q G:\Blog\Hexo_Lite\Shoka\source\_posts
 rmdir /s /q G:\Blog\Hexo_Lite\Fluid\source\_posts
+rmdir /s /q G:\Blog\Hexo_Lite\Next\source\_posts
 
 md G:\Blog\Hexo_Lite\Butterfly\source\_posts
 md G:\Blog\Hexo_Lite\Shoka\source\_posts
 md G:\Blog\Hexo_Lite\Fluid\source\_posts
-
-xcopy /s /q G:\Blog\Hexo\source\_posts G:\Blog\Article\Normal
+md G:\Blog\Hexo_Lite\Next\source\_posts
 
 xcopy /s /q G:\Blog\Article\Normal G:\Blog\Hexo_Lite\Butterfly\source\_posts
-xcopy /s /q G:\Blog\Article\Shoka G:\Blog\Hexo_Lite\Fluid\source\_posts
-xcopy /s /q G:\Blog\Article\Shoka G:\Blog\Hexo_Lite\Shoka\source\_posts
+xcopy /s /q G:\Blog\Article\NoLabel G:\Blog\Hexo_Lite\Fluid\source\_posts
+xcopy /s /q G:\Blog\Article\NoLabelG:\Blog\Hexo_Lite\Shoka\source\_posts
+xcopy /s /q G:\Blog\Article\NoLabel G:\Blog\Hexo_Lite\Next\source\_posts
 
 pause
