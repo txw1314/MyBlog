b706eaf3e1f056106cf5dd869c02a50472f089fe
上杉九月 <13562008871@163.com>
2022-05-16 20:47:15 +0800

    fix bugs

--- 彻底清除文件.bat
+++ 彻底清除文件.bat
@@ -16,7 +16,7 @@ md G:\Blog\Hexo_Lite\Next\source\_posts
 
 xcopy /s /q G:\Blog\Article\Normal G:\Blog\Hexo_Lite\Butterfly\source\_posts
 xcopy /s /q G:\Blog\Article\NoLabel G:\Blog\Hexo_Lite\Fluid\source\_posts
-xcopy /s /q G:\Blog\Article\NoLabelG:\Blog\Hexo_Lite\Shoka\source\_posts
+xcopy /s /q G:\Blog\Article\NoLabel G:\Blog\Hexo_Lite\Shoka\source\_posts
 xcopy /s /q G:\Blog\Article\NoLabel G:\Blog\Hexo_Lite\Next\source\_posts
 
 pause
