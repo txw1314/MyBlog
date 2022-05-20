417423c97eab5e75fa20cc58720e18f4bc6bf601
上杉九月 <13562008871@163.com>
2022-05-20 20:39:42 +0800

    fix bugs

--- 一键部署文章.bat
+++ 一键部署文章.bat
@@ -16,15 +16,6 @@ hexo g
 md G:\Blog\Hexo\themes\volantis\source\Fluid
 xcopy /s /q G:\Blog\Hexo_Lite\Fluid\public\ G:\Blog\Hexo\themes\volantis\source\Fluid
 
-
-cd G:\Blog\Hexo_Lite\Next
-hexo clean
-hexo bangumi -u
-hexo cinema -u
-hexo g
-md G:\Blog\Hexo\themes\volantis\source\Next
-xcopy /s /q G:\Blog\Hexo_Lite\Next\public\ G:\Blog\Hexo\themes\volantis\source\Next
-
 cd G:\Blog\Hexo_Lite\Shoka
 hexo clean
 hexo bangumi -u
